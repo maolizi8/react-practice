9cdfb619e24cbccfeeed034f37b5f7787ca0558f
maolizi8 <635961515@163.com>
2017-03-28 23:39:53 +0800

    201703282339

--- react-dev/src/components/baidu.js
+++ react-dev/src/components/baidu.js
@@ -1,8 +1,8 @@
 require('styles/reset.css')
 import React from 'react';
 import Header from './baidu/header/header'
-import Topnav from './baidu/header/topnav';
 import Section from './baidu/section/section';
+//import Topnav from './baidu/header/topnav';'<Topnav />'
 
 //React.initializeTouchEvents(true)
 //注意，事件的回调被绑定在了 react 组件上，而不是原始的元素上。React 通过一个 autobinding 过程自动将方法绑定到当前的组件实例上。
@@ -11,7 +11,7 @@ class Baidu extends React.Component {
    render() {
     return (
     	<div>
-    	<Topnav />
+    	
     	  <Header location='北京' />
     	  <Section />
     	</div>
--- react-dev/src/components/baidu/section/section.js
+++ react-dev/src/components/baidu/section/section.js
@@ -1,10 +1,14 @@
 require('components/baidu/section/section.css');
+
 import React from 'react';
 
 class Section extends React.Component {
+  
   constructor(props){
      super(props);
+     var imgSrc=require('components/baidu/section/news2.02.jpg');
     this.state={
+      defaultImg:imgSrc,
       newsList:[{title:'你真的愿意多花689元买个红色版iPhone 7 吗？',source:'新浪微博',time:'08:43',img:['images/new01.jpg','images/new02.jpg','images/new03.jpg']},
                 {title:'丁俊晖微博开炮:被强行阻止参赛 那一刻简直气疯',source:'网易新闻',time:'13:11',img:[]},
                 {title:'考上清华不会游泳不能毕业 , “旱鸭子”们炸锅了……',source:'myzaker',time:'00:03',img:['images/news2.01.jpg','images/news2.02.jpg','images/news2.03.jpg']},
@@ -25,7 +29,7 @@ class Section extends React.Component {
               <div className='img_sm'>
                 {imglist.map(function(imgs){
                     return (
-                      <div className='img_sm_item'><img src={imgs} /></div>
+                      <div className='img_sm_item'><img src={imgs||this.state.defaultImg} /></div>
                     )
                   }
                   )
